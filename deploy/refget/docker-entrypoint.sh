130de4c7a86fb7409cf91edaa901bd78d884d999
almutlue <almut.lue@gmail.com>
2025-02-17 16:45:49 +0100

    feat(deploy): Add basic refegt server with default settings

---
+++ deploy/refget/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+# only use entrypoint if running refget-server
+if [ "$1" = "refget-server" ] ; then
+
+  # Templating
+  if [ -e "/refget/config.properties" ]; then
+    echo "Using existing config.properties"
+
+  elif [ -e "/refget/config.properties.template" ]; then
+    echo "Generating config.properties from config.properties.template"
+    envsubst < /refget/config.properties.template > /refget/config.properties
+    cat /refget/config.properties
+    echo "$@"
+
+  else
+    echo "No config.properties or config.properties.template found. Exiting."
+    exit 1
+
+  fi
+fi
+
+exec "$@"
