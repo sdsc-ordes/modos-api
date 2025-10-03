9e8c6f2fda5b2576dc94df53433404a3363338b3
cmdoret <cyril.mattheydoret@gmail.com>
2025-10-03 15:24:25 +0200

    test(cli): add remote tests with server mocking

--- tests/test_cli_remote.py
+++ tests/test_cli_remote.py
@@ -4,13 +4,17 @@ from typer.testing import CliRunner
 
 from modos.cli import cli
 import pytest
+from pytest_httpserver import HTTPServer
 
 runner = CliRunner()
 
 
 @pytest.mark.slow
-def test_create_modo_auth(setup):
+def test_create_modo_auth(setup, httpserver: HTTPServer):
     minio_endpoint = setup["minio"].get_config()["endpoint"]
+    httpserver.expect_request("/").respond_with_json(
+        {"status": "success", "s3": f"http://{minio_endpoint}"}
+    )
     result = runner.invoke(
         cli,
         [
@@ -21,26 +25,26 @@ def test_create_modo_auth(setup):
         ],
         env={
             "AWS_ACCESS_KEY_ID": "minioadmin",
-            "AWS_SECRET": "minioadmin",
-            "MODOS_ENDPOINT": f"http://{minio_endpoint}",
+            "AWS_SECRET_ACCESS_KEY": "minioadmin",
+            "MODOS_ENDPOINT": httpserver.url_for("/"),
         },
     )
     assert result.exit_code == 0
 
 
 @pytest.mark.slow
-def test_show_modo_anon(setup):
-    endpoint = setup["minio"].get_config()["endpoint"]
+def test_list_modo(httpserver: HTTPServer):
+    httpserver.expect_request("/list").respond_with_json(
+        {"modos": ["s3://test/ex"]}
+    )
     result = runner.invoke(
         cli,
         [
             "--endpoint",
-            f"http://{endpoint}",
+            httpserver.url_for("/"),
             "--anon",
-            "create",
-            "-m",
-            '{"id":"test", "creation_date": "2024-05-14", "last_update_date": "2024-05-14"}',
-            "s3://test/ex",
+            "remote",
+            "list",
         ],
     )
     assert result.exit_code == 0
