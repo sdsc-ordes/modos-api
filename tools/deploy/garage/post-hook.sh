dff87bd5e777004147d4408f5d7a9fac4ba5b24c
almutlue <almut.lue@gmail.com>
2025-11-17 10:20:53 +0100

    fix: add env variables to specify bucket names

--- tools/deploy/garage/post-hook.sh
+++ tools/deploy/garage/post-hook.sh
@@ -3,6 +3,8 @@
 sleep 5
 garage layout assign -z dc1 -c 1G $(garage status | tail -n1 | cut -f1 -d' ' )
 garage layout apply --version 1
-garage bucket create modos-demo
+garage bucket create ${S3_BUCKET}
+garage bucket create ${REFERENCE_BUCKET}
 garage key import --yes ${CLIENT_ID} ${CLIENT_SECRET} -n demo
-garage bucket allow --read --write --owner modos-demo --key ${CLIENT_ID}
+garage bucket allow --read --write --owner ${S3_BUCKET} --key ${CLIENT_ID}
+garage bucket allow --read --write --owner ${REFERENCE_BUCKET} --key ${CLIENT_ID}
